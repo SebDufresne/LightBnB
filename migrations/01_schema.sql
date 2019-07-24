5c5e0cb7385c9f0ecdf67ab694a62258a5403d85
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-07-24 19:31:38 +0000

    Created Seeds01

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE lightbnb;
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   name TEXT,
@@ -13,7 +15,6 @@ CREATE TABLE properties (
   thumbnail_photo_url TEXT,
   cover_photo_url TEXT,
   cost_per_night INTEGER,
-  street TEXT,
   parkings_spaces INTEGER,
   number_of_bathrooms SMALLINT,
   number_of_bedrooms SMALLINT,
@@ -22,7 +23,7 @@ CREATE TABLE properties (
   city TEXT,
   province TEXT,
   post_code TEXT,
-  active BOOLEAN,
+  active BOOLEAN
 );
 
 CREATE TABLE reservations (
@@ -30,7 +31,7 @@ CREATE TABLE reservations (
   start_date DATE,
   end_date DATE,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
-  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE property_reviews (
